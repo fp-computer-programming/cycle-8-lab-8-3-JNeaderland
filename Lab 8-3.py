13f83924eda84e3af84aff97b9181c6edecb7675
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-12-04 12:06:39 -0500

    Add files via upload

---
+++ Lab 8-3.py
@@ -0,0 +1,11 @@
+#Author: Jacob Neaderland
+def sum_to(n):
+    i = 1
+    #Iteration Number
+    #First time its 1, then its 
+    while i < n:
+        i = i+1
+        a = i
+        a = (a * (a + 1))/2
+    print (a)
+sum_to(10)
